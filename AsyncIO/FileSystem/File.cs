1bb611e93ccdb44143aa5bb96efac9561ca99d18
Mykhailo Matviiv <firenero94@gmail.com>
2016-01-14 16:14:28 +0200

    - Create AsyncIO project with FileSystem/File class that will implement asynchronous access to file system. - Create AppendAllLinesAsync declaration and overloads.

---
+++ AsyncIO/FileSystem/File.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncIO.FileSystem
+{
+    public static class File
+    {
+        public static Task AppendAllLinesAsync(string path, IEnumerable<string> contents)
+        {
+            return AppendAllLinesAsync(path, contents, Encoding.UTF8, CancellationToken.None);
+        }
+
+        public static Task AppendAllLinesAsync(string path, IEnumerable<string> contents, Encoding encoding)
+        {
+            return AppendAllLinesAsync(path, contents, encoding, CancellationToken.None);
+        }
+
+        public static Task AppendAllLinesAsync(string path, IEnumerable<string> contents, CancellationToken cancellationToken)
+        {
+            return AppendAllLinesAsync(path, contents, Encoding.UTF8, cancellationToken);
+        }
+
+        public static Task AppendAllLinesAsync(string path, IEnumerable<string> contents, Encoding encoding, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
